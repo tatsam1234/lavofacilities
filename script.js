ac595a339119f2a5994d0ec0e1b4c884cc2afd2f
tatsam <tatsam1234@gmail.com>
2022-02-28 12:40:39 +0545

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+ScrollReveal({ reset: true ,
+    distance :'60px',
+    duration:2500,
+    // delay:400
+    });
+    
+    ScrollReveal().reveal('.feature-container-sec', { delay: 100 , origin: 'right'});
+    ScrollReveal().reveal('.feature-container', { delay: 100 , origin: 'left'});
+    ScrollReveal().reveal('.first', { delay: 100 , origin: 'left'});
+    ScrollReveal().reveal('.second', { delay: 100 , origin: 'bottom'});
+    ScrollReveal().reveal('.third', { delay: 100 , origin: 'right'});
+ 
+ 
+    
